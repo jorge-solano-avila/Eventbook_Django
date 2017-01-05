272a890b83997fafa862d4d2c0f2209e9a66e611
jorge52an <jorgesolavila@gmail.com>
2017-01-05 17:51:37 -0500

    Chore: change url runserver

--- eventbook/settings.py
+++ eventbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "57_nt&_o-8ejbp7ehsmrw1sz46j!aj*_tr!^i-*v^ioou^g_u5"
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.11"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
