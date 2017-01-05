dde08326d9544a1e2049103ce63548fcbfd7d6f5
jorge52an <jorgesolavila@gmail.com>
2017-01-05 00:04:22 -0500

    Fix: add corsheaders and rest framework

--- events/views.py
+++ events/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from models import City, Event
 
-# Create your views here.
+
