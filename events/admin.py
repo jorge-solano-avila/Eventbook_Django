21ec4f9f1cee9127cea7de29a2a332d318b86cfe
jorge52an <jorgesolavila@gmail.com>
2017-01-05 11:03:25 -0500

    Feat: add admin views

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from events.models import City, Event
 
-# Register your models here.
+
+class CityAdmin( admin.ModelAdmin ):
+	search_fields = ["id", "placeId", "name"]
+	list_display = ( "id", "placeId", "name" )
+
+class EventAdmin( admin.ModelAdmin ):
+	search_fields = ["id", "name", "type", "latitude", "longitude"]
+	raw_id_fields = ["city"]
+	list_display = ( "id", "name", "type", "description", "city", "startDateTime", "finishDateTime", "latitude", "longitude" )
+
+admin.site.register( City, CityAdmin )
+admin.site.register( Event, EventAdmin )
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+class UserAdmin( admin.ModelAdmin ):
+	search_fields = ["id", "name", "lastName", "email"]
+	list_display = ( "id", "name", "lastName", "email" )
+
+admin.site.register( User, UserAdmin )
