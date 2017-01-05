131be35b2ed2f2d587c8bd9c362ca5c5ee2af11a
jorge52an <jorgesolavila@gmail.com>
2017-01-04 23:25:29 -0500

    Feat: add Event and City models

--- events/models.py
+++ events/models.py
@@ -1,5 +1,29 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class City( models.Model ):
+	name = name = models.CharField( db_column = "Name", max_length = 255, null = False, blank = False )
+
+	def __unicode__( self ):
+        return self.name
+
+class Event( models.Model ):
+	TYPE_CHOICES = (
+		( "CO", "Concert" ),
+		( "SP", "Sport" ),
+		( "CI", "Cinema" ),
+		( "TH", "Theater" ),
+		( "PR", "Programming" ),
+		( "OT", "Other" ),
+	)
+
+	name = models.CharField( db_column = "Name", max_length = 255, null = False, blank = False )
+	type = models.CharField( db_column = "Type", max_length = 2, choices = TYPE_CHOICES, default = "OT" )
+	description = models.TextField( db_column = "Description", null = True, blank = True )
+	city = models.ForeignKey( City )
+	startDateTime = models.DateTimeField( db_column = "StartDateTime", null = False, blank = False )
+	finishDateTime = models.DateTimeField( db_column = "FinishDateTime", null = False, blank = False )
+
+	def __unicode__( self ):
+        return str( self.id ) + " - " + self.name
