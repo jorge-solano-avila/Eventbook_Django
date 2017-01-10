48b344b6b2ff2a2833badacfeff732cfd5738aa1
jorge52an <jorgesolavila@gmail.com>
2017-01-10 17:27:05 -0500

    Chore: configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eventbook.wsgi --log-file -
--- eventbook/settings.py
+++ eventbook/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname( os.path.dirname( os.path.abspath( __file__ ) ) )
+PROJECT_ROOT = os.path.dirname( os.path.abspath( __file__ ) )
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +27,7 @@ SECRET_KEY = "57_nt&_o-8ejbp7ehsmrw1sz46j!aj*_tr!^i-*v^ioou^g_u5"
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.11"]
+# ALLOWED_HOSTS = ["192.168.0.11"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -37,15 +39,17 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
     "events",
-    "users"
+    "users",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -69,6 +73,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "debug": DEBUG
         },
     },
 ]
@@ -124,7 +129,27 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config( conn_max_age = 500 )
+DATABASES["default"].update( db_from_env )
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ( "HTTP_X_FORWARDED_PROTO", "https" )
+
+# Allow all host headers
+ALLOWED_HOSTS = ["*"]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_ROOT = os.path.join( PROJECT_ROOT, "staticfiles" )
 STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join( PROJECT_ROOT, "static" ),
+]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
